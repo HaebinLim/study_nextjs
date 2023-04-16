91feeeb994354892101ec83cd285a6cc315d0213
HaebinLim <goguma-guma@hanmail.net>
2023-04-16 21:16:45 +0900

    코딩애플 - Next.js로 웹서비스 만들기

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import Link from 'next/link'
 
 export const metadata = {
   title: 'Create Next App',
@@ -7,8 +8,14 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="ko">
+      <body>
+      <nav className="flex gap-x-[20px] p-[20px] text-black  bg-white">
+        <Link href="/">Home</Link>
+        <Link href="/list">List</Link>
+      </nav>
+        {children}
+      </body>
     </html>
   )
 }
---
+++ app/list/page.js
@@ -0,0 +1,13 @@
+export default function List() {
+  return (
+    <div>
+      <h1 className="mt-[50px] text-center">상품 목록</h1>
+      <div className="my-[20px] mx-auto p-[20px] w-[200px] rounded-[5px] text-black bg-white">
+        <h2>상품1</h2>
+      </div>
+      <div className="my-[20px] mx-auto p-[20px] w-[200px] rounded-[5px] text-black bg-white">
+        <h2>상품2</h2>
+      </div>
+    </div>
+  )
+}
